ef1101a95be38186c2bf15583ba31cce1d6a8b17
carlosjac312 <115487243+carlosjac312@users.noreply.github.com>
2023-09-29 17:06:40 +0200

    Update main_test.ts

--- main_test.ts
+++ main_test.ts
@@ -2,7 +2,8 @@ import { assertEquals } from "https://deno.land/std@0.203.0/assert/mod.ts";
 import { add } from "./main.ts";
 
 Deno.test(function addTest() {
-  /*type pasajero = {
+  //Space speed APARTADO 1
+  type pasajero = {
     nombre?:string,
     origen?:string,
     altura:number,
@@ -11,12 +12,14 @@ Deno.test(function addTest() {
     género:string
     infectado:boolean;
   }
+  //Space speed APARTADO 2
   interface nave  {
     peso:number,
     dimensiones:number[],
     velocidad_curva:number,
     listado_pasajeros:pasajero[]
   }
+  ////Space speed APARTADO 3
   const pasajero1:pasajero={
     nombre:"Jose",
     origen:"Granada",
@@ -51,36 +54,43 @@ Deno.test(function addTest() {
     género:"hombre",
     infectado:true
   }
-  let datosEnterprise:nave={
+  let datosEnterprise:nave={//DATO MODIFICABLE
     peso:4000000,
     dimensiones:[4,4,4,4],
     velocidad_curva:30,
     listado_pasajeros:[pasajero1,pasajero2,pasajero3,pasajero4]
   }
+  //Space speed APARTADO 4
   datosEnterprise.listado_pasajeros.forEach(element => {
     if(Object.keys(element).find(e=>e==="nombre")) console.log(Object.values(element));
   });
+  
+  //This Side of Paradise APARTADO 1
   datosEnterprise.listado_pasajeros.forEach((elem:pasajero)=>{
     if(!elem.infectado) console.log(elem.nombre);
   })
+  //This Side of Paradise APARTADO 2
   const alguienInfect = datosEnterprise.listado_pasajeros.some((p)=>p.infectado)
   const todosSanos = datosEnterprise.listado_pasajeros.every((p)=>!p.infectado)
   const primerInfect = datosEnterprise.listado_pasajeros.find((p)=>p.infectado)
   console.log(`Infectado detectado: ${alguienInfect}\nToda la tripulacion sana: ${todosSanos}\nPrimer infectado detectado: ${primerInfect?.nombre}`)
   
-  
+  //The City on the Edge of Forever APARTADO 1
   const fechas = [1234, 22, -6, 234.5];
-  const fechasEntendibles: number[]=fechas.filter((e)=>e>=0&&!e.toString().includes('.'));
+  const fechasEntendibles: number[]=fechas.filter((e)=>e>=0&&!e.toString().includes('.'));//ESTO LO HAGO PARA EVITAR NUMEROS NEGATIVOS Y DECIMALES
   console.log(fechasEntendibles)
-  function getRandomInt(min: number, max: number): number {
+  //The City on the Edge of Forever APARTADO 2
+  function getRandomInt(min: number, max: number): number {//FUNCION PARA GENERAR NUMEROS ALEATORIOS ENTRE DOS DE ELLOS
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   const fechasPerfectas = fechasEntendibles.map((elem:number)=>{
     return "Año: "+elem+"|Mes: "+getRandomInt(1,12)+"|Dia: "+getRandomInt(1,30)+"|Hora: "+getRandomInt(0,23)+"|Minuto: "+getRandomInt(0,59)+"|Segundo: "+getRandomInt(0,59);
-  });
-  console.log(fechasPerfectas);*/
+  });//AQUI ES DONDE USA LA FUNCION DE NUMEROS ALEATORIOS PARA QUE ME PONGA LOS DATOS A RELLENAR SOLO
+  console.log(fechasPerfectas);
+
+  //The Trouble with Tribbles
   type turbo_conducto = {
     numero:number
     edad:number
@@ -93,9 +103,10 @@ Deno.test(function addTest() {
     {numero:3, edad:20, daños:8, tribbles:50},
     {numero:4, edad:10, daños:0, tribbles:12},
   ]
+  //GENERACIÓN DE NUMERO TOTAL DE TRIBBLES EN TUBOS DE MAS DE 20 AÑOS
   const mayor20 = listaTubos.filter((e)=>e.edad>20).map((e)=>e.tribbles).reduce((a,e)=>a+=e);
   console.log(mayor20)
-  const señal = listaTubos.forEach((e)=>console.log("Numero: "+e.numero+"| Daños:"+e.daños+"| Tribbles:"+e.tribbles));
-  const peligro=listaTubos.map((e)=> e.tribbles>e.daños/2 ?true:false)
+  const señal = listaTubos.forEach((e)=>console.log("Numero: "+e.numero+"| Daños:"+e.daños+"| Tribbles:"+e.tribbles));//SEÑAL CON NUMERO, DAÑOS Y TRIBBLES
+  const peligro=listaTubos.map((e)=> e.tribbles>e.daños/2 ?true:false)//ENVIO SEÑAL DE PELIGRO
   console.log(peligro)
 });
